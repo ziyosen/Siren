2b0c076ee99a0dd13f036c6f23fe7f4c4350a37c
ziyosen <ziyosenstore@gmail.com>
2026-05-20 01:08:52 +0700

    Change route from '/free/:proxyip' to '/Benxx-Project/:proxyip'

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@ async fn main(req: Request, env: Env, _: Context) -> Result<Response> {
     Router::with_data(config)
         .on_async("/", fe)
         .on_async("/free/cc/:proxyip", tunnel)
-        .on_async("/free/:proxyip", tunnel)
+        .on_async("/Benxx-Project/:proxyip", tunnel)
         .on_async("/:proxyip", tunnel)
         .run(req, env)
         .await
